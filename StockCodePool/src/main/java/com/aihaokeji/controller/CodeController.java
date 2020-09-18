1da78c1b42d1fcc5140ab810cef9413bab0ec9ec
zhanghhvip <abcxian@163.com>
2020-09-18 10:24:42 +0800

    stockpool接口

--- StockCodePool/src/main/java/com/aihaokeji/controller/CodeController.java
+++ StockCodePool/src/main/java/com/aihaokeji/controller/CodeController.java
@@ -1,9 +1,16 @@
 package com.aihaokeji.controller;
 
 
+import com.aihaokeji.entity.Code;
+import com.aihaokeji.service.CodeService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * <p>
@@ -16,6 +23,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/code")
 public class CodeController {
+	@Autowired
+	private CodeService codeService;
+	@RequestMapping("/findall")
+	public List<String> findall(){
+		QueryWrapper<Code> queryWrapper = new QueryWrapper<>();
+		queryWrapper.select("code");
+		List<Code> codeList = codeService.list(queryWrapper);
+		List<String> list = new ArrayList<>(codeList.size());
+		for(Code c : codeList){
+			list.add(c.getCode());
+		}
+		return list;
+	}
 
 }
 
